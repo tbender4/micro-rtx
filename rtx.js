6a25bfe1528982a87213d244e5ddbfcb81e34008
Thomas Bender <thomas.bender82@qmail.cuny.edu>
2021-02-25 15:02:04 -0500

    fixed writing changes. broken upon reload

--- rtx.js
+++ rtx.js
@@ -15,11 +15,14 @@ let products = {};
 
 function decrease(key) {
   products[key].count = products[key].count - 1;
-  //TODO: make this async
-  fs.writeFile('products.json', products, (err) => {
-    if (err) return;
-    console.log('Wrote', key, 'to disk.');
-    return;
+  //TODO: see if sync/async is a problem
+  fs.writeFile('products.json', JSON.stringify(products), (err) => {
+    if (err) {
+      console.log('Failed to write to disk');
+    }
+    else {
+    console.log('Wrote', key, 'to disk. Quantity is now', products[key].count);
+    }
   });
 }
 
