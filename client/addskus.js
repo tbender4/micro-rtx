af4a483858d5f41674d01875084c90c8ec41a41b
Thomas Bender <thomas.bender82@qmail.cuny.edu>
2021-03-02 03:38:28 -0500

    fixed date

--- client/addskus.js
+++ client/addskus.js
@@ -1,8 +1,7 @@
-var date = Date.now();
-let todayDateFormatted = date.getMonth();
+let todayDate = new Date(); //gets today's date
+let todayDateFormatted = todayDate.getMonth();
 
 let date_div = document.getElementById("current-date");
 let date_text = document.createTextNode(todayDateFormatted);
 
 date_div.appendChild(date_text);
-
