7944ac0692112923f288f476959344d0fa85c8a6
Thomas Bender <thomas.bender82@qmail.cuny.edu>
2021-03-11 12:54:45 -0500

    broken version

--- client/addskus.js
+++ client/addskus.js
@@ -16,3 +16,15 @@ date_div.appendChild(date_text);
 //practicing addEventListner as alternative to onclick()
 let btn = document.getElementById("search-sku");
 btn.addEventListener("click", postAddSku);
+
+let confirmSKUTr = document.getElementByID("confirm-sku-tr");
+
+document getSKUInfo() {
+  //returns TR info
+  let skuTDs = confirmSKUTr.childNodes;
+  skuTDs[0].innerHTML = "sku-here";
+  skuTDs[1].innerHTML = "img-here";
+  skuTDs[2].innerHTML = "name-here";
+  skuTDs[3].innerHTML = "pricehere";
+  skuTDs[4].innerHTML = "modelhere";
+}
