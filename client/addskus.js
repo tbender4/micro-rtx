69f9bbc7519559b49a68d5136634de8791aafb42
Thomas Bender <thomas.bender82@qmail.cuny.edu>
2021-03-11 17:42:56 -0500

    basic add-sku div functionality

--- client/addskus.js
+++ client/addskus.js
@@ -1,9 +1,23 @@
 //used for both managing and adding skus
 
-function postAddSku() {
-    
-    let userSKU = document.getElementById("req-sku").value;
-    console.log("Adding ", userSKU);
+
+function getSKUInfo() {
+  //changes all TDs info
+  
+  let userSKU = document.getElementById("req-sku").value;
+  let confirmSKUTr = document.getElementById("confirm-sku-tr");
+
+  let skuTDs = confirmSKUTr.getElementsByTagName("td");
+  console.log(skuTDs.length)
+  console.log(skuTDs)
+  skuTDs[0].innerHTML = "sku-here";
+  skuTDs[1].innerHTML = "img-here";
+  skuTDs[2].innerHTML = "name-here";
+  skuTDs[3].innerHTML = "pricehere";
+  skuTDs[4].innerHTML = "modelhere";
+
+  let quantityDiv = document.getElementById("confirm-area")
+  quantityDiv.hidden = false;
 }
 
 let todayDate = new Date(); //gets today's date
@@ -15,16 +29,6 @@ date_div.appendChild(date_text);
 
 //practicing addEventListner as alternative to onclick()
 let btn = document.getElementById("search-sku");
-btn.addEventListener("click", postAddSku);
+btn.addEventListener("click", getSKUInfo);
 
-let confirmSKUTr = document.getElementByID("confirm-sku-tr");
 
-document getSKUInfo() {
-  //returns TR info
-  let skuTDs = confirmSKUTr.childNodes;
-  skuTDs[0].innerHTML = "sku-here";
-  skuTDs[1].innerHTML = "img-here";
-  skuTDs[2].innerHTML = "name-here";
-  skuTDs[3].innerHTML = "pricehere";
-  skuTDs[4].innerHTML = "modelhere";
-}
